faf17fa63dacb50aa54ed3e68bacc6fa309584e9
nikunj_38 <bholuprajapati3883@gmail.com>
2021-03-17 20:33:37 +0530

    -update product with ios setup done(8.33 PM 17 March 2021)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sqflite_app/Constants/Constants.dart';
-import 'package:sqflite_app/Screens/ListOfProduct.dart';
 import 'package:sqflite_app/Screens/SplashScreen.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.white, // navigation bar color
     statusBarColor: Colors.transparent, // status bar color
-    statusBarBrightness: Brightness.dark,//status bar brigtness
+    statusBarBrightness: Brightness.dark,//status bar brightness
     statusBarIconBrightness:Brightness.dark , //status barIcon Brightness
     systemNavigationBarDividerColor: Colors.greenAccent,//Navigation bar divider color
     systemNavigationBarIconBrightness: Brightness.light, //navigation bar icon
